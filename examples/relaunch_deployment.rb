e0e89516752c0a33e6ebe943ef6925246211456a
jeremyd <4072+jeremyd@users.noreply.github.com>
2010-01-05 11:01:34 -0800

    --only option, for selective relaunch, growl support doesn't work quite yet

--- examples/relaunch_deployment.rb
+++ examples/relaunch_deployment.rb
@@ -5,11 +5,14 @@ require 'rest_connection'
 
 opts = Trollop::options do
   opt :deployment, "deployment nickname", :type => :string, :required => true
+  opt :only, "regex string matching the nickname of the servers you want to relaunch. This excludes servers that do not match\nExample --only ubuntu", :type => :string, :required => false
+  opt :growl, "use growl notification (string) when servers are all operational, requires ruby-growl gem", :type => :string, :required => false
 end
 
 # find all servers in the deployment (the fast way)
 deployment = Deployment.find_by_nickname_speed(opts[:deployment]).first
 servers = deployment.servers
+servers = servers.select { |s| s.nickname =~ /#{opts[:only]}/ } if opts[:only]
 servers.each do |s|
   # force the server to populate it's state
   s.reload
@@ -26,3 +29,14 @@ end
 servers.each do |s|
   s.start
 end
+
+#if opts[:growl]
+#  require 'ruby-growl'
+#  servers.each do |s|
+#    s.wait_for_state('booting')
+#  end
+#  g = Growl.new "localhost", "ruby-growl",
+#              ["ruby-growl Notification"]
+#g.notify "ruby-growl Notification", "It Came From Ruby-Growl",
+#         "Greetings!"
+#end
