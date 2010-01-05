80d42fabc68c0cf0681a7d6fe4b1ce38008ac33d
Jeremy Deininger <jeremy@rightscale.com>
2010-01-05 14:30:14 -0800

    reving version, minor mods to tools, reloading server settings during reload operations.

--- examples/dev_setup.rb
+++ examples/dev_setup.rb
@@ -3,6 +3,7 @@ require 'rubygems'
 require 'trollop'
 require 'rest_connection'
 require 'net/ssh'
+require 'ruby-debug'
 
 opts = Trollop::options do
   opt :deployment, "deployment nickname", :type => :string, :required => true
@@ -12,10 +13,18 @@ end
 
 # find all servers in the deployment (the fast way)
 deployment = Deployment.find_by_nickname_speed(opts[:deployment]).first
-servers = deployment.servers
+servers = deployment.servers_no_reload
 servers = servers.select { |s| s.nickname =~ /#{opts[:only]}/ } if opts[:only]
 
 servers.each do |s|
+  s.wait_for_state('operational')
+end
+
+puts "sleeping 20 just in case"
+sleep 20 
+
+servers.each do |s|
+  s.reload
   Net::SSH.start(s.dns_name, 'root', :keys => ['~/.ssh/publish-test']) do |ssh|
     puts "setting up devmode: dropbox::install && devmode::setup_cookbooks"
     puts ssh.exec!("rs_run_recipe -n 'dropbox::install'")
