0ec72f68b7902741dd68c19087cdd66839464bfb
Jeremy Deininger <jeremy@rightscale.com>
2010-01-05 19:02:33 -0800

    we should wait for dns AND wait for state operational

--- examples/dev_setup.rb
+++ examples/dev_setup.rb
@@ -16,28 +16,19 @@ servers = deployment.servers_no_reload
 servers = servers.select { |s| s.nickname =~ /#{opts[:only]}/ } if opts[:only]
 
 servers.each do |s|
+  while(1)
+    puts "waiting for dns-name for #{s.nickname}"
+    break if s['dns-name'] && !s['dns-name'].empty?
+    s.reload
+    sleep 2
+  end
+  puts "DNS: #{s['dns-name']}"
+
   s.wait_for_state('operational')
-end
-
-puts "sleeping 20 just in case"
-sleep 20 
-
-servers.each do |s|
-  s.reload
+  
   Net::SSH.start(s.dns_name, 'root', :keys => ['~/.ssh/publish-test']) do |ssh|
     puts "setting up devmode: dropbox::install && devmode::setup_cookbooks"
     puts ssh.exec!("rs_run_recipe -n 'dropbox::install'")
     puts ssh.exec!("rs_run_recipe -n 'devmode::setup_cookbooks'")
   end
 end
-
-#if opts[:growl]
-#  require 'ruby-growl'
-#  servers.each do |s|
-#    s.wait_for_state('booting')
-#  end
-#  g = Growl.new "localhost", "ruby-growl",
-#              ["ruby-growl Notification"]
-#g.notify "ruby-growl Notification", "It Came From Ruby-Growl",
-#         "Greetings!"
-#end
