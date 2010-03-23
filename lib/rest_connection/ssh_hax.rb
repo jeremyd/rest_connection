f2ca2024d49b69992e948adb9f6e073652d2526d
jeremy <jeremy@domU-12-31-39-0F-CD-51.compute-1.internal>
2010-03-23 18:17:46 +0000

    re-adding v4 compatibility run_script method, renaming v5 script runner to run_executable

--- lib/rest_connection/ssh_hax.rb
+++ lib/rest_connection/ssh_hax.rb
@@ -93,8 +93,8 @@ module SshHax
   end
 
   # script is an Executable object with minimally nick or id set
-  def run_script(script, options={}, ssh_key=nil)
-    raise "FATAL: run_script called on a server with no dns_name. You need to run .settings on the server to populate this attribute." unless self.dns_name
+  def run_executable(script, options={}, ssh_key=nil)
+    raise "FATAL: run_executable called on a server with no dns_name. You need to run .settings on the server to populate this attribute." unless self.dns_name
 
     raise "FATAL: unrecognized format for script.  Must be an Executable or RightScript with href or name attributes" unless (script.is_a?(RightScript) || script.is_a?(Executable)) && (script.href || script.name)
     if script.href
