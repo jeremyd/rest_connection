cda7f0c58eabc1697c1fcfd913912b903fc2ac30
jeremyd <4072+jeremyd@users.noreply.github.com>
2010-05-19 10:08:35 -0700

    image jockey spec passes; new warning message for when json.load fails to parse the http response body

--- spec/image_jockey.rb
+++ spec/image_jockey.rb
@@ -7,23 +7,25 @@ describe MultiCloudImageInternal, "exercises the mci internal api" do
 
   it "should do some stuff" do
     some_image_href = "https://moo1.rightscale.com/api/acct/0/ec2_images/ami-0859bb61?cloud_id=1"
-    some_st_id = 59180
     @mci = MultiCloudImageInternal.create(:name => "123deleteme-test test 1234", :description => "woah")
+    @mci2 = MultiCloudImageInternal.create(:name => "1234deleteme-test test 12345", :description => "woah")
     @new_setting = MultiCloudImageCloudSettingInternal.create(:multi_cloud_image_href => @mci.href, :cloud_id => 1, :ec2_image_href => some_image_href, :aws_instance_type => "m1.small")
 
-    #@new_st = ServerTemplate.find(some_st_id)
-    @new_st = "/api/acct/2901/server_templates/59180"
+    @new_st = ServerTemplate.create(:multi_cloud_image_href => @mci.href, :nickname => "123deleteme-test test 123456", :description => "1234")
 
-    @really_new_st = ServerTemplateInternal.new(:href => @new_st)
-    @really_new_st.add_multi_cloud_image(@mci.href)
-    @really_new_st.set_default_multi_cloud_image(@mci.href)
-    @really_new_st.multi_cloud_images
+    @really_new_st = ServerTemplateInternal.new(:href => @new_st.href)
+    @really_new_st.add_multi_cloud_image(@mci2.href)
+    @really_new_st.set_default_multi_cloud_image(@mci2.href)
+    trash = @really_new_st.multi_cloud_images
+    trash.class.should == Array
+    trash.first.class.should == Hash
     @really_new_st.delete_multi_cloud_image(@mci.href)
   end
 
   after(:all) do
+    @new_st.destroy
     #@mci.destroy
-    #@new_st.destroy
+    #@mci2.destroy
   end
   
 
