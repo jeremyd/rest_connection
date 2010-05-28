4aaee4da4aed4446f0c5e0b7a674408c1cfea7b0
jeremyd <4072+jeremyd@users.noreply.github.com>
2010-05-28 15:52:19 -0700

    removing old examples and cleanup

--- examples/relaunch_deployment.rb
+++ examples/relaunch_deployment.rb
@@ -6,40 +6,22 @@ require 'rest_connection'
 opts = Trollop::options do
   opt :deployment, "deployment nickname", :type => :string, :required => false
   opt :only, "regex string matching the nickname of the servers you want to relaunch. This excludes servers that do not match\nExample --only ubuntu", :type => :string, :required => false
-  opt :growl, "use growl notification (string) when servers are all operational, requires ruby-growl gem", :type => :string, :required => false
   opt :id, "deployment id", :type => :string, :required => false
 end
 
 # find all servers in the deployment (the fast way)
 if opts[:id]
-  deployment = Deployment.find_by_id(opts[:id])
+  deployment = Deployment.find(opts[:id])
 else
   deployment = Deployment.find_by_nickname_speed(opts[:deployment]).first
 end
 servers = deployment.servers_no_reload
 servers = servers.select { |s| s.nickname =~ /#{opts[:only]}/ } if opts[:only]
 servers.each do |s|
-  # send stop
-  s.relaunch
+  s.stop
+end
+servers.each do |s|
+  s.wait_for_state("stopped")
+  s.start
 end
 
-# wait for termination
-#servers.each do |s|
-#  s.wait_for_state('stopped')
-#end
-
-# relaunch
-#servers.each do |s|
-#  s.start
-#end
-
-#if opts[:growl]
-#  require 'ruby-growl'
-#  servers.each do |s|
-#    s.wait_for_state('booting')
-#  end
-#  g = Growl.new "localhost", "ruby-growl",
-#              ["ruby-growl Notification"]
-#g.notify "ruby-growl Notification", "It Came From Ruby-Growl",
-#         "Greetings!"
-#end
