96accf64621b22a7daafa77bfb2d3ab8640511fb
Tim Rodriguez <timothy@rightscale.com>
2011-01-18 17:30:17 -0800

    more multicloud support + bugfixes

--- lib/rest_connection/rightscale/deployment.rb
+++ lib/rest_connection/rightscale/deployment.rb
@@ -13,12 +13,70 @@
 #    You should have received a copy of the GNU General Public License
 #    along with RestConnection.  If not, see <http://www.gnu.org/licenses/>.
 
-class Deployment 
-  include RightScale::Api::Base
-  extend RightScale::Api::BaseExtend
+class Deployment
+  class McDeployment
+    include RightScale::Api::Gateway
+    extend RightScale::Api::GatewayExtend
+  
+    def resource_plural_name
+      "deployments"
+    end 
+
+    def resource_singular_name
+      "deployment"
+    end 
+
+    def self.resource_plural_name
+      "deployments"
+    end 
+
+    def self.resource_singular_name
+      "deployment"
+    end
+  end
+
+  class EC2Deployment
+    include RightScale::Api::Base
+    extend RightScale::Api::BaseExtend
+  
+    def resource_plural_name
+      "deployments"
+    end 
+
+    def resource_singular_name
+      "deployment"
+    end 
+
+    def self.resource_plural_name
+      "deployments"
+    end 
+
+    def self.resource_singular_name
+      "deployment"
+    end
+  end
+
+  def initialize(params = {})
+    if params[:cloud_id].to_i < 10
+      @deploy = Deployment::EC2Deployment.new(params)
+    else
+      @deploy = Deployment::McDeployment.new(params)
+    end
+  end
+
+  def method_missing(method_name, *args)
+    @deploy.method_missing(method_name, *args)
+  end
+
+  def create(opts)
+    location = connection.post(@deploy.resource_plural_name, @deploy.resource_singular_name.to_sym => opts)
+    newrecord = self.new('href' => location)
+    newrecord.reload
+    newrecord
+  end
 
   def set_input(name, value)
-    deploy_href = URI.parse(self.href)
+    deploy_href = URI.parse(@deploy.href)
     connection.put(deploy_href.path, :deployment => {:parameters => {name => value} })
   end
 
@@ -46,7 +104,7 @@ class Deployment
   end
 
   def clone
-    deploy_href = URI.parse(self.href)
+    deploy_href = URI.parse(@deploy.href)
     Deployment.new(:href => connection.post(deploy_href.path + "/duplicate"))
   end
 end
